0222cd5c71aef42912cc6534978c159f096e8263
SUMMAIYA <103142498+Summaiya-Begum@users.noreply.github.com>
2022-11-10 01:57:50 +0530

    connection set up with database

--- heavens-basket/pages/api/hello.js
+++ heavens-basket/pages/api/hello.js
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default async function handler(req, res) {
+import mongoConnection from "../../config/db"
 
+export default async function handler(req, res) {
+mongoConnection()
+console.log("Hi welcome")
   res.status(200).json({ name: 'John Doe' });
 }
