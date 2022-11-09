b8c160e28aa7b5a4bf4335513070d0fd39635e38
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-09 16:24:10 +0530

    Init

--- heavens-basket/components/Navbar.jsx
+++ heavens-basket/components/Navbar.jsx
@@ -1,20 +1,57 @@
+import {
+  Box,
+  Button,
+  HStack,
+  Image,
+  Input,
+  Select,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
-import { Box, HStack, Image, VStack } from "@chakra-ui/react";
+import { Search2Icon, StarIcon, TriangleDownIcon } from "@chakra-ui/icons";
+import { BsCart4 } from "react-icons/bs";
+import { FaPencilAlt } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
-    // <Box>
-    <HStack>
-      <Image src="https://www.naturesbasket.co.in/Images/logosnew.png?v=2" />
-      <VStack>
-        <HStack>
-          <Text>Onlile slots Availability |</Text>
-          <Text>fresh & Fast |</Text>
+    <Box display="flex" m={"auto"} border="1px">
+      <Image src="Logo.png" w={"28%"} h="120px" />
+      <Box w={"63%"}>
+        <HStack m={"10px"} ml={"5%"}>
+          <Text>Online Slots Available |</Text>
+          <Text>Fresh & Fast |</Text>
           <Text>Store Locator |</Text>
           <Text>Contact Us |</Text>
         </HStack>
-      </VStack>
-    </HStack>
-    // </Box>
+        <Box display={"flex"}>
+          <Button>400001</Button>
+          <Select w={"28%"}>
+            <option value="Mumbai">Mumbai</option>
+            <option value="Pune">Pune</option>
+            <option value="Bangalore">Bangalore</option>
+            <option value="Delhi NCR">Delhi NCR</option>
+            <option value="Thane">Thane</option>
+            <option value="Kolkata">Kolkata</option>
+          </Select>
+          <Input textAlign={"center"} placeholder="Start shopping..." />
+          <Button bgColor={"#3e9d15"} color="white">
+            <Search2Icon />
+          </Button>
+        </Box>
+      </Box>
+      <Box w="9%">
+        <Text>
+          Welcome Jyoti <TriangleDownIcon boxSize={"10px"} />
+        </Text>
+        <Box mt={"20px"} ml={"10px"}>
+          <Button bgColor={"white"} color={"#3e9d15"}>
+            <StarIcon fontSize={"25px"} />
+          </Button>
+          <Button bgColor={"white"} color={"#3e9d15"}>
+            <BsCart4 fontSize={"25px"} />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 };
--- heavens-basket/pages/index.js
+++ heavens-basket/pages/index.js
@@ -1,12 +1,9 @@
 import { Box } from "@chakra-ui/react";
-import Head from "next/head";
-import Image from "next/image";
 import { Navbar } from "../components/Navbar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Box>
+    <Box w={"95%"} m={"auto"}>
       <Navbar />
     </Box>
   );
