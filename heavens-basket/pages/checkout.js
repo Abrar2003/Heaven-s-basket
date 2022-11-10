63610dd148ad7a790982a4fb2629e6451df696c7
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-11 02:38:38 +0530

    Checkout selected slot Page

--- heavens-basket/pages/checkout.js
+++ heavens-basket/pages/checkout.js
@@ -5,8 +5,11 @@ import {
   Divider,
   FormControl,
   FormLabel,
+  Grid,
+  GridItem,
   Heading,
   Input,
+  Radio,
   Select,
   Tab,
   TabList,
@@ -19,6 +22,36 @@ import React from "react";
 import Footer from "../components/footer/Footer";
 import { Navbar } from "../components/navbar/Navbar";
 import styles from "../styles/Checkout.module.css";
+
+const slots = [
+  { disable: true, radio: false, time: "Today" },
+  { disable: true, radio: true, time: "Self Pickup" },
+  { disable: true, radio: true, time: "Fresh & Fast" },
+  { disable: true, radio: true, time: "09.00 am - 11.00 am" },
+  { disable: false, radio: true, time: "11.00 am - 01.00 pm" },
+  { disable: false, radio: true, time: "02.00 pm - 04.00 pm" },
+  { disable: false, radio: true, time: "05.00 pm - 07.00 pm" },
+  { disable: false, radio: true, time: "Same Day Delivery" },
+  { disable: false, radio: true, time: "07.00 pm - 09.00 pm" },
+  { disable: true, radio: false, time: "Tomorrow" },
+  { disable: true, radio: true, time: "Self Pickup" },
+  { disable: true, radio: true, time: "Fresh & Fast" },
+  { disable: false, radio: true, time: "09.00 am - 11.00 am" },
+  { disable: false, radio: true, time: "11.00 am - 01.00 pm" },
+  { disable: false, radio: true, time: "02.00 pm - 04.00 pm" },
+  { disable: false, radio: true, time: "05.00 pm - 07.00 pm" },
+  { disable: false, radio: true, time: "Same Day Delivery" },
+  { disable: false, radio: true, time: "07.00 pm - 09.00 pm" },
+  { disable: true, radio: false, time: "Sun 13-11-2022" },
+  { disable: true, radio: true, time: "Self Pickup" },
+  { disable: true, radio: true, time: "Fresh & Fast" },
+  { disable: false, radio: true, time: "09.00 am - 11.00 am" },
+  { disable: false, radio: true, time: "11.00 am - 01.00 pm" },
+  { disable: false, radio: true, time: "02.00 pm - 04.00 pm" },
+  { disable: false, radio: true, time: "05.00 pm - 07.00 pm" },
+  { disable: false, radio: true, time: "Same Day Delivery" },
+  { disable: false, radio: true, time: "07.00 pm - 09.00 pm" },
+];
 
 const checkout = () => {
   const add = false;
@@ -176,10 +209,66 @@ const checkout = () => {
                 </Box>
               </TabPanel>
             )}
-            {/* <TabPanel>
-              <p>two!</p>
-            </TabPanel>
+
             <TabPanel>
+              <Text>SELECT DELIVERY SLOT</Text>
+              <Box
+                display={"flex"}
+                justifyContent={"space-between"}
+                mb={"10px"}
+              >
+                <Text>Selected slot : Today: 11.00am-01.00am</Text>
+                <Button bg={"#92be4d"} color={"white"}>
+                  PROCEED TO PAY
+                </Button>
+              </Box>
+
+              <Box>
+                <Grid
+                  templateColumns="repeat(9, 1fr)"
+                  gap={"1px"}
+                  fontSize={"14px"}
+                >
+                  {slots.map((elem, i) => {
+                    return elem.radio === false ? (
+                      <GridItem bg={"rgb(220, 206, 206)"}>
+                        <Text>{elem.time}</Text>
+                      </GridItem>
+                    ) : elem.disable === true ? (
+                      <GridItem bg={"grey"}>
+                        <Radio isDisabled>{elem.time}</Radio>
+                      </GridItem>
+                    ) : (
+                      <GridItem bg={"white"}>
+                        <Radio colorScheme="green">{elem.time}</Radio>
+                      </GridItem>
+                    );
+                  })}
+                </Grid>
+              </Box>
+              <Box
+                display={"flex"}
+                justifyContent={"space-between"}
+                mt={"10px"}
+              >
+                <Box display={"flex"} gap={"10px"}>
+                  <Checkbox size="lg" colorScheme="white" defaultChecked>
+                    Available
+                  </Checkbox>
+                  <Checkbox size="lg" colorScheme="gray" defaultChecked>
+                    Not Available
+                  </Checkbox>
+                  <Checkbox size="lg" colorScheme="green" defaultChecked>
+                    Selected
+                  </Checkbox>
+                </Box>
+                <Button bg={"#92be4d"} color={"white"}>
+                  PROCEED TO PAY
+                </Button>
+              </Box>
+            </TabPanel>
+
+            {/*<TabPanel>
               <p>two!</p>
             </TabPanel>
             <TabPanel>
