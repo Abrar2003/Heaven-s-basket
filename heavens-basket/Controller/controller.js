6f8384badf5c71104c2b577003c4ddbad7e31cf9
SUMMAIYA <103142498+Summaiya-Begum@users.noreply.github.com>
2022-11-10 21:20:09 +0530

    product-put

--- heavens-basket/Controller/controller.js
+++ heavens-basket/Controller/controller.js
@@ -23,9 +23,6 @@ export async function postProduct(req,res){
     try{
         const postProd = req.body;
         if(!postProd)return res.statu(404).json({error:'error'})
-        // ProductModel.create(postProd,function(err,data){
-        //   return res.status(200).json(data)
-        // }) 
         const newProduct= ProductModel(postProd);
         await newProduct.save()
         console.log(newProduct)
@@ -35,3 +32,32 @@ export async function postProduct(req,res){
         return res.status(404).json({error})
     }
 } 
+
+// put : http://localhost:3000/api/product
+export async function putProduct(req,res){
+  console.log(req.param.id)
+try{  
+  // const {productId} = req.query
+  // const productData = req.body
+  // if(productId && productData){
+  //  const updateProduct= ProductModel.findByIdAndUpdate(productId,productData);
+  //   await updateProduct.save()
+  //   console.log(updateProduct)
+
+  //  res.status(200).json({updateProduct})
+  //  }
+  res.status(2002).json(req.params._id)
+}catch(error){
+  res.status(404).json({error:"Error While Updating the Data"})
+}
+}
+
+export async function deleteProduct(req,res){
+  console.log(req.query.id)
+try{
+  await ProductModel.findByIdAndDelete({_id:req.query.id})
+  res.send("User Deleted Success")
+}catch(error){
+  res.status(404).json({error:"Error While Updating the Data"})
+}
+}
--- heavens-basket/pages/api/products/index.js
+++ heavens-basket/pages/api/products/index.js
@@ -1,31 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import mongoConnection from "../../../config/db"
-import {getProduct , postProduct} from"../../../Controller/controller"
+import mongoConnection from "../../../config/db";
+import {
+  getProduct,
+  postProduct,
+  deleteProduct,
+  putProduct
+} from "../../../Controller/controller";
+// import { deleteProduct, putProduct } from "./[id]";
 export default async function handler(req, res) {
-mongoConnection().catch(()=>res.status(405).json({error:"Error"}))
-console.log("Hi Products")
+  mongoConnection().catch(() => res.status(405).json({ error: "Error" }));
+  console.log("Hi Products");
 
-// type of request
-const {method} = req
-switch(method){
-  case "GET":
-    getProduct(req,res)
-    // res.status(200).json({method, name:'GET Request' });
-    break;
-  case "POST":
-    postProduct(req,res)
-    // res.status(200).json({method,name:"POST Reqeust"});
-    break;
-  case "PUT":
-    res.status(200).json({method,name:"PUT Request"});
-    break;
-  case "DELETE":
-    res.status(200).json({method,name: "DELETE Requst"});
-    break;
-  default : 
-  // res.setHeader("Allow",["GET","PUT","POST","DELETE"])
-  // res.status(405).end(`MEthod ${method}Not Allowed`)
-}
+  // type of request
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      getProduct(req, res);
+      // res.status(200).json({method, name:'GET Request' });
+      break;
+    case "POST":
+      postProduct(req, res);
+      // res.status(200).json({method,name:"POST Reqeust"});
+      break;
+    case "PUT":
+    putProduct(req,res)
+      // res.status(200).json({method,name:"PUT Request"});
+      break;
+    case "DELETE":
+      deleteProduct(req,res)
+      // res.status(200).json({method,name: "DELETE Requst"});
+      break;
+    default:
+      // res.setHeader("Allow",["GET","PUT","POST","DELETE"])
+      res.status(405).end(`MEthod ${method}Not Allowed`);
+  }
   // res.status(200).json({ name: 'Products Route' });
 }
