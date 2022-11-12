d7acd9e4b262130ff8120541a1f166f4906338fc
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-12 16:23:27 +0530

    Index page modified

--- heavens-basket/pages/home.js
+++ heavens-basket/pages/home.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 //React-Icons
 import { IconContext } from "react-icons";
 import { AiFillCaretRight } from "react-icons/ai";
@@ -74,130 +73,267 @@ export default function home() {
           </Box>
         </Flex>
         <IconContext.Provider value={{ color: "black", size: "10px" }}>
-
-        <Flex gap={".5%"} alignItems={"center"} justifyContent="center">
-          <Divider borderColor={"black"} />
-          <Box w={["80%", "70%", "65%", "60%", "54%"]}>
-            <Text
-              w="100%"
-              p={"none"}
-              textAlign={"center"}
-              fontSize={["xs", "md", "xl", "2xl", "3xl"]}
-            >
-              SHOP BY CATAGORY
-            </Text>
-          </Box>
-          <Divider borderColor={"black"} />
-        </Flex>
-
-        <Flex justifyContent={"space-evenly"}>
-          <Stack w={"24%"}>
-            <Box>
-              <Image
-                className={style.box}
-                mb="10px"
-                rounded={["5px", "10px", "10px", "15px", "20px"]}
-                w={"100%"}
-                src="veggies-card.png"
-              />
-              <Text fontSize={"large"}>Exotic Vegitables</Text>
+          <Flex gap={".5%"} alignItems={"center"} justifyContent="center">
+            <Divider borderColor={"black"} />
+            <Box w={["80%", "70%", "65%", "60%", "54%"]}>
+              <Text
+                w="100%"
+                p={"none"}
+                textAlign={"center"}
+                fontSize={["xs", "md", "xl", "2xl", "3xl"]}
+              >
+                SHOP BY CATAGORY
+              </Text>
             </Box>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Asparagus Artichokes</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Avocados Peppers</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Broccoli Zucchini</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Text color={"green.500"}>veiw all</Text>
-          </Stack>
+            <Divider borderColor={"black"} />
+          </Flex>
 
-          <Stack w={"24%"}>
-            <Box>
-              <Image
-                className={style.box}
-                mb="10px"
-                rounded={["5px", "10px", "10px", "15px", "20px"]}
-                w={"100%"}
-                src="meat-card.png"
-              />
-              <Text fontSize={"large"}>Meat, Poultry and Seafood</Text>
-            </Box>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Chicken Cuts Lollipops Mince</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Fish Other Seafood</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Fresh Kebabs Others</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Text color={"green.500"}>veiw all</Text>
-          </Stack>
+          <Flex justifyContent={"space-evenly"}>
+            <Stack w={"24%"}>
+              <Box>
+                <Image
+                  className={style.box}
+                  mb="10px"
+                  rounded={["5px", "10px", "10px", "15px", "20px"]}
+                  w={"100%"}
+                  src="veggies-card.png"
+                />
+                <Text fontSize={"large"}>Exotic Vegitables</Text>
+              </Box>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Asparagus Artichokes
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Avocados Peppers
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Broccoli Zucchini
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Text color={"green.500"}>veiw all</Text>
+            </Stack>
 
-          <Stack w={"24%"}>
-            <Box>
-              <Image
-                className={style.box}
-                mb="10px"
-                rounded={["5px", "10px", "10px", "15px", "20px"]}
-                w={"100%"}
-                src="bread-card.png"
-              />
-              <Text fontSize={"large"}>Fresh Artisanal Breads</Text>
-            </Box>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Breadsticks Crostinni Lavache</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Buns Croissants Bagels</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Muffins Cakes Brownies</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Text color={"green.500"}>veiw all</Text>
-          </Stack>
+            <Stack w={"24%"}>
+              <Box>
+                <Image
+                  className={style.box}
+                  mb="10px"
+                  rounded={["5px", "10px", "10px", "15px", "20px"]}
+                  w={"100%"}
+                  src="meat-card.png"
+                />
+                <Text fontSize={"large"}>Meat, Poultry and Seafood</Text>
+              </Box>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Chicken Cuts Lollipops Mince
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Fish Other Seafood
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Fresh Kebabs Others
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Text color={"green.500"}>veiw all</Text>
+            </Stack>
 
-          <Stack w={"24%"}>
-            <Box>
-              <Image
-                className={style.box}
-                mb="10px"
-                rounded={["5px", "10px", "10px", "15px", "20px"]}
-                w={"100%"}
-                src="snacks-card.png"
-              />
-              <Text fontSize={"large"}>Daily Essentials</Text>
-            </Box>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Snacks & Beverages</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Breakfast, Dairy & Bakery</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Flex pr={"10%"} alignItems={"center"} justifyContent={"space-between"}>
-              <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>Staples</Text>
-              <AiFillCaretRight />
-            </Flex>
-            <Text color={"green.500"}>veiw all</Text>
-          </Stack>
-        </Flex>
+            <Stack w={"24%"}>
+              <Box>
+                <Image
+                  className={style.box}
+                  mb="10px"
+                  rounded={["5px", "10px", "10px", "15px", "20px"]}
+                  w={"100%"}
+                  src="bread-card.png"
+                />
+                <Text fontSize={"large"}>Fresh Artisanal Breads</Text>
+              </Box>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Breadsticks Crostinni Lavache
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Buns Croissants Bagels
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Muffins Cakes Brownies
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Text color={"green.500"}>veiw all</Text>
+            </Stack>
+
+            <Stack w={"24%"}>
+              <Box>
+                <Image
+                  className={style.box}
+                  mb="10px"
+                  rounded={["5px", "10px", "10px", "15px", "20px"]}
+                  w={"100%"}
+                  src="snacks-card.png"
+                />
+                <Text fontSize={"large"}>Daily Essentials</Text>
+              </Box>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Snacks & Beverages
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Breakfast, Dairy & Bakery
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Flex
+                pr={"10%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Text fontSize={["7px", "9px", "11px", "13px", "15px"]}>
+                  Staples
+                </Text>
+                <AiFillCaretRight />
+              </Flex>
+              <Text color={"green.500"}>veiw all</Text>
+            </Stack>
+          </Flex>
         </IconContext.Provider>
       </Stack>
+      <Flex gap={".5%"} alignItems={"center"} justifyContent="center">
+        <Divider borderColor={"black"} />
+        <Box w={["80%", "70%", "65%", "60%", "54%"]}>
+          <Text
+            w="100%"
+            p={"none"}
+            textAlign={"center"}
+            fontSize={["xs", "md", "xl", "2xl", "3xl"]}
+          >
+            WHAT'S TERNDING
+          </Text>
+        </Box>
+        <Divider borderColor={"black"} />
+      </Flex>
       <MultiSlider />
+      <Flex gap={".5%"} alignItems={"center"} justifyContent="center">
+        <Divider borderColor={"black"} />
+        <Box w={["80%", "70%", "65%", "60%", "54%"]}>
+          <Text
+            w="100%"
+            p={"none"}
+            textAlign={"center"}
+            fontSize={["xs", "md", "xl", "2xl", "3xl"]}
+          >
+            OUR EVENTS
+          </Text>
+        </Box>
+        <Divider borderColor={"black"} />
+      </Flex>
+      <Flex w="100%" justifyContent={"space-evenly"}>
+        <Box
+          w="30%"
+          rounded={["5px", "10px", "10px", "15px", "20px"]}
+          className={style.box}
+        >
+          <Image
+            w="100%"
+            rounded={["5px", "10px", "10px", "15px", "20px"]}
+            src="event1.jpg"
+            alt="banner"
+          />
+        </Box>
+        <Box
+          w="30%"
+          rounded={["5px", "10px", "10px", "15px", "20px"]}
+          className={style.box}
+        >
+          <Image
+            w="100%"
+            rounded={["5px", "10px", "10px", "15px", "20px"]}
+            src="event2.jpg"
+            alt="banner"
+          />
+        </Box>
+        <Box
+          w="30%"
+          rounded={["5px", "10px", "10px", "15px", "20px"]}
+          className={style.box}
+        >
+          <Image
+            w="100%"
+            rounded={["5px", "10px", "10px", "15px", "20px"]}
+            src="event3.png"
+            alt="event"
+          />
+        </Box>
+      </Flex>
     </Box>
   );
 }
