a3f432e3ad67bf692e25a911f24aeda655db462c
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-13 18:01:45 +0530

    Cart page

--- heavens-basket/pages/cart.js
+++ heavens-basket/pages/cart.js
@@ -59,97 +59,103 @@ import { NEXT_URL } from "../lib/helper";
 // ];
 
 const Cart = () => {
-  const [ total, setTotal ] = useState( 0 );
-  const [ data, setData ] = useState( [] );
+  const [total, setTotal] = useState(0);
+  const [data, setData] = useState([]);
 
-  let url = `${ NEXT_URL }/api/cart/`;
+  let url = `${NEXT_URL}/api/cart/`;
   let emails = "abrar.aalam003@gmail.com";
-  useEffect( () => {
+  useEffect(() => {
     fetchData();
-  }, [] );
+  }, []);
 
   const fetchData = async () => {
-    let daa = await axios.get( url, {
+    let daa = await axios.get(url, {
       headers: {
         email: emails,
       },
-    } );
-    setData( daa.data );
+    });
+    setData(daa.data);
   };
 
-  useEffect( () => {
-    const t = data.reduce( ( acc, el ) => {
+  useEffect(() => {
+    const t = data.reduce((acc, el) => {
       return acc + el.price * el.qty;
-    }, 0 );
-    setTotal( t );
-  }, [ data ] );
+    }, 0);
+    setTotal(t);
+  }, [data]);
 
-  const handleQuantity = async ( { id, qty } ) => {
-    console.log( id, qty )
-    await axios.patch( url, { id: id, qty: qty } ).then( ( res ) => {
-      fetchData();
-    } ).catch( ( e ) => {
-      console.log( e );
-    } )
-  }
+  const handleQuantity = async ({ id, qty }) => {
+    console.log(id, qty);
+    await axios
+      .patch(url, { id: id, qty: qty })
+      .then((res) => {
+        fetchData();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   ////delete
-  const handleDelete = async ( _id ) => {
+  const handleDelete = async (_id) => {
     await axios
-      .delete( url, {
+      .delete(url, {
         headers: {
           productid: _id,
         },
-      } )
-      .then( () => fetchData() )
-      .catch( ( e ) => {
-        console.log( e );
-      } );
+      })
+      .then(() => {
+        fetchData();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
-  const changeQty = ( id, q ) => {
-    const newData = data.map( ( el ) =>
+  const changeQty = (id, q) => {
+    const newData = data.map((el) =>
       id === el.id ? { ...el, qty: el.qty + q } : el
     );
     // console.log(newData);
-    setData( newData );
+    setData(newData);
   };
 
   // console.log("datttttta", data);
+
   return (
     <Stack>
       <Navbar />
-      <Box w={ "100%" } p="0% 2%">
+      <Box w={"100%"} p="0% 2%">
         <Image
-          w={ "100%" }
-          mb={ "20px" }
+          w={"100%"}
+          mb={"20px"}
           src="https://gnbdevcdn.s3-ap-southeast-1.amazonaws.com/Marketing/40ffdf36-959e-49f1-9c5c-e43639e69a01.png?v=4"
         />
-        <Box display={ "flex" } justifyContent={ "space-between" } mb={ "10px" }>
-          <Box display={ "flex" } alignItems="center" gap={ "20px" }>
+        <Box display={"flex"} justifyContent={"space-between"} mb={"10px"}>
+          <Box display={"flex"} alignItems="center" gap={"20px"}>
             <Text>MY CART(LENGTH OF CART) |</Text>
-            <Box gap={ "5px" }>
-              <RepeatIcon color={ "#92be4d" } fontSize={ "20px" } /> Refresh
+            <Box gap={"5px"}>
+              <RepeatIcon color={"#92be4d"} fontSize={"20px"} /> Refresh
             </Box>
           </Box>
-          <Box display={ "flex" } gap={ "20px" }>
-            <Button bg={ "#92be4d" } color={ "white" }>
+          <Box display={"flex"} gap={"20px"}>
+            <Button bg={"#92be4d"} color={"white"}>
               <Link href="/">CONTINUE SHOPPING</Link>
             </Button>
-            <Button bg={ "#92be4d" } color={ "white" }>
+            <Button bg={"#92be4d"} color={"white"}>
               <Link href="/checkout">PROCEED TO CHECKOUT</Link>
             </Button>
           </Box>
         </Box>
         <Box
-          display={ "flex" }
+          display={"flex"}
           justifyContent="center"
-          gap={ "10px" }
-          alignItems={ "center" }
-          borderTop={ "2px solid #dedede" }
+          gap={"10px"}
+          alignItems={"center"}
+          borderTop={"2px solid #dedede"}
         >
-          <TbTruckDelivery fontSize={ "25px" } />
-          <Text color={ "#b71761" } fontWeight={ "semibold" }>
+          <TbTruckDelivery fontSize={"25px"} />
+          <Text color={"#b71761"} fontWeight={"semibold"}>
             GET YOUR ORDER DELIVERED IN 90 MINUTES . SERVICE TIMINGS BETWEEN
             10:00 AM TO 9.30 PM .
           </Text>
@@ -158,54 +164,52 @@ const Cart = () => {
           <Table variant="simple">
             <Thead>
               <Tr>
-                <Th w={ "40%" }>Name</Th>
-                <Th w={ "14%" }>Price</Th>
-                <Th w={ "14%" }>Discount</Th>
-                <Th w={ "14%" }>Qty</Th>
-                <Th w={ "14%" }>Sub-Total</Th>
-                <Th w={ "4%" }></Th>
+                <Th w={"40%"}>Name</Th>
+                <Th w={"14%"}>Price</Th>
+                <Th w={"14%"}>Discount</Th>
+                <Th w={"14%"}>Qty</Th>
+                <Th w={"14%"}>Sub-Total</Th>
+                <Th w={"4%"}></Th>
               </Tr>
             </Thead>
 
             <Tbody>
-              { data.map( ( el, i ) => {
+              {data.map((el, i) => {
                 return (
                   <Counter
-
-                    key={ i }
-                    { ...el }
-                    changeQty={ changeQty }
-                    i={ i }
-                    handleDelete={ handleDelete }
-                    handleQuantity={ handleQuantity }
-
+                    key={i}
+                    {...el}
+                    changeQty={changeQty}
+                    i={i}
+                    handleDelete={handleDelete}
+                    handleQuantity={handleQuantity}
                   />
                 );
-              } ) }
+              })}
             </Tbody>
           </Table>
         </TableContainer>
-        <Box align={ "right" }>
-          <Box w={ "20%" } align="left" p={ "10px" }>
-            <Text>Sub-Total: ₹ { total }.00</Text>
+        <Box align={"right"}>
+          <Box w={"20%"} align="left" p={"10px"}>
+            <Text>Sub-Total: ₹ {total}.00</Text>
             <Text>Delivery Charges: ₹ 0.00</Text>
-            <Text fontSize="lg">Total: ₹ { total }.00</Text>
+            <Text fontSize="lg">Total: ₹ {total}.00</Text>
           </Box>
         </Box>
         <Box
-          align={ "right" }
-          borderTop={ "1px solid #dedede" }
-          borderBottom={ "2px solid #dedede" }
-          p={ "10px 0px 10px 0px" }
+          align={"right"}
+          borderTop={"1px solid #dedede"}
+          borderBottom={"2px solid #dedede"}
+          p={"10px 0px 10px 0px"}
         >
-          <Button bg={ "#92be4d" } color={ "white" }>
+          <Button bg={"#92be4d"} color={"white"}>
             <Link href="/">CONTINUE SHOPPING</Link>
           </Button>
-          <Button ml={ "20px" } bg={ "#92be4d" } color={ "white" }>
+          <Button ml={"20px"} bg={"#92be4d"} color={"white"}>
             <Link href="/checkout">PROCEED TO CHECKOUT</Link>
           </Button>
         </Box>
-        <Text textAlign={ "center" } color={ "#b71761" } fontWeight={ "semibold" }>
+        <Text textAlign={"center"} color={"#b71761"} fontWeight={"semibold"}>
           RESTOCK YOUR BASKET
         </Text>
         <CartSlider />
