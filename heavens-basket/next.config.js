dff53199340ca4f65fd061b1473cf7520f25b6d8
Abrar2003 <spartanabrar@gmail.com>
2022-11-13 22:51:36 +0530

    update

--- heavens-basket/next.config.js
+++ heavens-basket/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
- 
+ env: {
+  NEXT_PUBLIC_FRONTEND_URL: "https://annoyed-spark-4097-6yv7.vercel.app"
+ }
 }
 
 
