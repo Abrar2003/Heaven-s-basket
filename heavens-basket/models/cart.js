19358059e4a68dc3f6d02d11aa19880cc3d98377
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-13 15:22:25 +0530

    Cart page

--- heavens-basket/models/cart.js
+++ heavens-basket/models/cart.js
@@ -1,21 +1,25 @@
-import { Schema, models, model } from 'mongoose';
+import { Schema, models, model } from "mongoose";
 import mongoose from "mongoose";
-import ProductModel from './products';
-
-
+import ProductModel from "./products";
 
 /// http://localhost:3000/api/cart
-const cartSchema = new Schema( {
+const cartSchema = new Schema(
+  {
     productId: {
-        type: mongoose.ObjectId, ref: ProductModel, unique: true, required: true
+      type: mongoose.ObjectId,
+      ref: ProductModel,
+      unique: true,
+      required: true,
     },
     email: {
-        type: String, required: true
-    }
-
-}, {
+      type: String,
+      required: true,
+    },
+  },
+  {
     versionKey: false,
-} )
-const cart = models.cart || new model( "cart", cartSchema )
+  }
+);
+const cart = models.cart || new model("cart", cartSchema);
 
 export default cart;
