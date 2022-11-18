a50ed3adbff3ff5d89971497eebf51109b1b0693
Prashant3216 <pv3216@gmail.com>
2022-11-18 15:28:20 +0530

    casual

--- heavens-basket/utils/connection.js
+++ heavens-basket/utils/connection.js
@@ -2,5 +2,6 @@ import mongoose from "mongoose"
 require("dotenv").config()
 export default async function dbConnect(){
     // await mongoose.connect("mongodb://127.0.0.1:27017/heavensBasket")
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect("mongodb+srv://summaiya:fathima@cluster0.jawkqoc.mongodb.net/HeavensBasket?retryWrites=true&w=majority")
+    console.log("connected")
 }
